0cac0ddfc6cbf8d9e25c7c36ce38e2e4d552a7f8
Giovanny Piovesan <giovannywrp@hotmail.com>
2017-04-15 15:43:00 -0300

    changed expo package version, now with native-base components

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
+import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'native-base';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Open up main.js to start working on your app!</Text>
+        <Button block large><Text>Hi</Text></Button>
       </View>
     );
   }
@@ -21,3 +22,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+Expo.registerRootComponent(App);
